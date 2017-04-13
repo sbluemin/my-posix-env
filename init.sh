8841d7a0415d6d6c1a896f4dca0196c2e20ae096
sbluemin <yoonhyeonmin@gmail.com>
2017-04-14 01:49:14 +0900

    자동 세팅 쉘 스크립트 파일 추가

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Init and update submodules..."
+
+# Update submodules
+git submodule init
+git submodule update
+
+echo "Setting vim configuration..."
+
+# 기존 홈 디렉토리에 존재하는 vim 관련 파일들 정리
+rm -rf ~/.vim ~/.vimrc
+
+# 업데이트 받은 설정 파일들을 심볼릭 링크를 이용하여 홈 디렉토리에 세팅
+ln -sf .vimrc ~/.vimrc
+ln -sf .vim ~/.vim
