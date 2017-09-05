d02798846055084c4871a0da56f77e365402caeb
윤 현민 <hyeonmin.yoon@netmarble.com>
2017-09-05 14:47:00 +0900

    ...

--- install-zsh.sh
+++ install-zsh.sh
@@ -2,8 +2,8 @@
 
 sudo apt install zsh
 
-sudo chsh -s `which zsh`
-
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -P /tmp/
 
 sudo sh /tmp/install.sh
+
+sudo chsh -s `which zsh`
