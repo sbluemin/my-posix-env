227b4fc63ef9528165daa2dd011854edcebc4bef
sbluemin <yoonhyeonmin@gmail.com>
2018-08-06 12:00:31 +0900

    zsh 설정 파일 삭제

--- install-zsh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt install zsh
-
-wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -P /tmp/
-
-sudo sh /tmp/install.sh
-
-sudo chsh -s `which zsh`
