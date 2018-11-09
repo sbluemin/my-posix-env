be6451a09ac3055dff607b6197617ec1ab4b39d5
sbluemin <yoonhyeonmin@gmail.com>
2018-11-09 16:06:32 +0900

    code로 다시 변경 ㅡ,ㅡ

--- run-mac.sh
+++ run-mac.sh
@@ -26,12 +26,16 @@ echo "Setting vim configuration..."
 rm -rf ~/.vim ~/.vimrc
 
 # 업데이트 받은 설정 파일들을 심볼릭 링크를 이용하여 홈 디렉토리에 세팅
-ln -sf $PWD/.vimrc ~/.vimrc
-ln -sf $PWD/vim/.vim ~/.vim
-ln -sf $PWD/.tmux.conf ~/.tmux.conf
-ln -sf $PWD/.gitconfig ~/.gitconfig
-ln -sf $PWD/.bashrc ~/.bash_profile
-ln -sf $PWD/.bash_aliases ~/.bash_aliases
+ln -sf $PWD ~/.sbluemin
+
+ln -sf ~/.sbluemin/vim/.vim ~/.vim
+
+ln -sf ~/.sbluemin/configs/.vimrc ~/.vimrc
+ln -sf ~/.sbluemin/configs/.tmux.conf ~/.tmux.conf
+ln -sf ~/.sbluemin/configs/.gitconfig ~/.gitconfig
+ln -sf ~/.sbluemin/configs/.bashrc ~/.bashrc
+ln -sf ~/.sbluemin/configs/.bashrc ~/.bash_profile
+ln -sf ~/.sbluemin/configs/.bash_aliases ~/.bash_aliases
 
 # 세팅 후 vim 플러그인 설치
 vim +PluginInstall +qall
