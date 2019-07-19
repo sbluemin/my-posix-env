3b2ce675b8f54702c714d74b5562dc6d9bcf5e56
sbluemin <yoonhyeonmin@gmail.com>
2019-07-20 04:08:54 +0900

    맥용 설치 스크립트 추가

---
+++ run-mac.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PWD= $(pwd)
+
+# 필수 패키지 설치
+brew install -f ripgrep ctags tig
+
+# Fuzzy finder 설치
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+    
+# Update submodules
+git submodule init
+git submodule update
+
+# 기존 홈 디렉토리에 존재하는 vim 관련 파일들 정리
+rm -rf ~/.vim ~/.vimrc
+rm -rf ~/.sbluemin
+
+# 업데이트 받은 설정 파일들을 심볼릭 링크를 이용하여 홈 디렉토리에 세팅
+ln -sf $PWD ~/.sbluemin
+
+ln -sf ~/.sbluemin/vim/.vim ~/.vim
+
+ln -sf ~/.sbluemin/configs/.vimrc ~/.vimrc
+ln -sf ~/.sbluemin/configs/.tmux.conf ~/.tmux.conf
+ln -sf ~/.sbluemin/configs/.tigrc ~/.tigrc
+ln -sf ~/.sbluemin/configs/.gitconfig ~/.gitconfig
+ln -sf ~/.sbluemin/configs/.bashrc ~/.bashrc
+ln -sf ~/.sbluemin/configs/.bashrc ~/.bash_profile
+ln -sf ~/.sbluemin/configs/.bash_aliases ~/.bash_aliases
+
+# 세팅 후 vim 플러그인 설치
+vim +PluginInstall +qall
+
+echo "Done!"
